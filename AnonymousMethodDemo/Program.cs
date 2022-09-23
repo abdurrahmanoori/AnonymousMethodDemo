c89616fe51bae5b58871419cab56ed66f66c9a32
Abdurrahman <Abdurrahmannoori10@gmail.com>
2022-09-23 18:55:32 +0430

    Add project files.

---
+++ AnonymousMethodDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnonymousMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
